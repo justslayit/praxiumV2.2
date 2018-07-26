71cb699a1013a67b18e24e76e12d12f76cc8515c
justslayit <41407164+justslayit@users.noreply.github.com>
2018-07-26 12:43:08 +0800

    Menu Bar (Mobile Ver.) Done
    Menu Bar (Mobile Ver.) Done
    Made slight changes to home page ".support img"

--- wp-content/themes/twentyseventeen/assets/js/slider.js
+++ wp-content/themes/twentyseventeen/assets/js/slider.js
@@ -5,4 +5,16 @@ jQuery(document).ready(function( $ ) {
         prevArrow: '<button type="button" class="slick-prev">&#x2039;</button>',
 
       });
+
+
+      $('.main-navigation').click(function() {
+      	if($(this).hasClass('toggled-on')) {
+      		$("html").addClass("menu-open");
+      		$(".menu-toggle").html('<svg class="icon icon-bars" aria-hidden="true" role="img"> <use href="#icon-bars" xlink:href="#icon-bars"></use> </svg><svg class="icon icon-close" aria-hidden="true" role="img"> <use href="#icon-close" xlink:href="#icon-close"></use> </svg> Close');
+      	} else {
+      		$("html").removeClass("menu-open");
+      		$(".menu-toggle").html('<svg class="icon icon-bars" aria-hidden="true" role="img"> <use href="#icon-bars" xlink:href="#icon-bars"></use> </svg><svg class="icon icon-close" aria-hidden="true" role="img"> <use href="#icon-close" xlink:href="#icon-close"></use> </svg> Menu');
+      	}
+      });
+
     });
